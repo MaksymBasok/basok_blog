24a76a4e1c9b0b884c1c1695572640384b7b0e8a
Dom <maxum245@gmail.com>
2025-06-10 13:25:02 +0300

    "ЛАБОРАТОРНА 5. ВАЛІДАЦІЯ ДАНИХ ТА ДОДАВАННЯ КАТЕГОРІЙ" ДО 10.06.25 15:30

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -2,15 +2,22 @@
 
 @section('content')
     @php /** @var \App\Models\BlogCategory $item */ @endphp
-    <form method="POST" action="{{ route('blog.admin.categories.update', $item->id) }}">
-        @method('PATCH')
+
+    <form method="POST"
+          action="{{ $item->exists
+                    ? route('blog.admin.categories.update', $item->id)
+                    : route('blog.admin.categories.store') }}">
         @csrf
+        @if ($item->exists)
+            @method('PATCH')
+        @endif
+
         <div class="container">
             @if ($errors->any())
                 <div class="row justify-content-center">
                     <div class="col-md-11">
                         <div class="alert alert-danger" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                             {{ $errors->first() }}
@@ -18,11 +25,12 @@
                     </div>
                 </div>
             @endif
+
             @if (session('success'))
                 <div class="row justify-content-center">
                     <div class="col-md-11">
                         <div class="alert alert-success" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                             {{ session()->get('success') }}
@@ -30,6 +38,7 @@
                     </div>
                 </div>
             @endif
+
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     @include('blog.admin.categories.includes.item_edit_main_col')
