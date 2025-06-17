9a1630e0f08c06056f3fd08ae52ced64963fe71b
Dom <maxum245@gmail.com>
2025-06-17 07:17:01 +0300

    Лабораторна 11. Колекції

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Blog\Admin\PostController;
 use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DiggingDeeperController;
+use App\Http\Controllers\Blog\Admin\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +21,13 @@ Route::middleware([
 });
 
 Route::resource('rest', RestTestController::class)->names('restTest');
+Route::group(['prefix' => 'digging_deeper'], function () {
+
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+        ->name('digging_deeper.collections');
+
+});
 
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
